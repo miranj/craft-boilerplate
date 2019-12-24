9e5bcba7dd27c6b398aa3f0da901199c2a4855bc
Prateek Rungta <prateekrungta@gmail.com>
2019-12-24 22:01:40 +0530

    Refactor Nginx Config
    - Extract env variables for better reuse
    - Add script to create symlinks
    - Improve file modularity
    - Improve folder structure

---
+++ serverconfigs/envs/production/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# 
+# Script to setup RunCloud config on a Miranj production server
+# 
+
+
+APP_NAME="boilerplate-craft"
+CDN_NAME="boilerplate-cdn"
+REDIRECTS_NAME="boilerplate-redirects"
+REPO_NAME="boilerplate"
+
+REPO_DIR="/home/runcloud/webapps/$APP_NAME/$REPO_NAME"
+TARGET_DIR="/etc/nginx-rc/extra.d"
+
+cd $TARGET_DIR
+
+# CDN
+ln -s $REPO_DIR/serverconfigs/nginx/cdn/static.conf $CDN_NAME.location.static.include.conf
+ln -s $REPO_DIR/serverconfigs/nginx/cdn/proxy.conf $CDN_NAME.location.proxy.include.conf
+
+# Craft
+ln -s $REPO_DIR/serverconfigs/nginx/craft/http.conf $APP_NAME.location.http.include.conf
+ln -s $REPO_DIR/serverconfigs/envs/production/env.conf $APP_NAME.location.main-before.include.conf
+ln -s $REPO_DIR/serverconfigs/nginx/craft/main.conf $APP_NAME.location.main.include.conf
+ln -s $REPO_DIR/serverconfigs/nginx/cdn/static.conf $APP_NAME.location.static.include.conf
+ln -s $REPO_DIR/serverconfigs/nginx/craft/proxy.conf $APP_NAME.location.proxy.include.conf
+
+# Redirects
+ln -s $REPO_DIR/serverconfigs/envs/production/env.conf $REDIRECTS_NAME.location.main-before.include.conf
+ln -s $REPO_DIR/serverconfigs/nginx/redirects/main.conf $REDIRECTS_NAME.location.main.include.conf
---
+++ serverconfigs/envs/staging/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 
+# Script to setup RunCloud config on the Miranj staging server
+# 
+
+
+APP_NAME="boilerplate"
+REPO_NAME="boilerplate"
+
+REPO_DIR="/home/runcloud/webapps/$APP_NAME/$REPO_NAME"
+TARGET_DIR="/etc/nginx-rc/extra.d"
+
+cd $TARGET_DIR
+ln -s $REPO_DIR/serverconfigs/envs/staging/env.conf $APP_NAME.location.main-before.10-env.conf
+ln -s $REPO_DIR/serverconfigs/envs/staging/auth.conf $APP_NAME.location.main-before.20-auth.conf
+ln -s $REPO_DIR/serverconfigs/nginx/cdn/static.conf $APP_NAME.location.static.include.conf
