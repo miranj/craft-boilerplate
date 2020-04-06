aad04705538766a6ecea216b834111da6c0a45ec
Prateek Rungta <prateekrungta@gmail.com>
2020-04-06 16:52:32 +0530

    Add retina, no-js variants to Tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,4 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+const postcss = require('postcss')
 
 let fonts = {
   title: defaultTheme.fontFamily.sans,
@@ -30,4 +32,23 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    plugin(function({ addVariant, e }) {
+      addVariant('retina', ({ container, separator }) => {
+        const retinaRule = postcss.atRule({ name: 'media', params: '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)' })
+        retinaRule.append(container.nodes)
+        container.append(retinaRule)
+        retinaRule.walkRules(rule => {
+          rule.selector = `.${e(`retina${separator}${rule.selector.slice(1)}`)}`
+        })
+      })
+    }),
+    plugin(function({ addVariant, e }) {
+      addVariant('no-js', ({ modifySelectors, separator }) => {
+        modifySelectors(({ selector }) => {
+          return `.has-no-js .${e(`no-js${separator}`)}${selector.slice(1)}`
+        })
+      })
+    }),
+  ],
 }
