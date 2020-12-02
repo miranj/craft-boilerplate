10db83e18d9542d5f2ed21600670a3ce0f3a8279
Prateek Rungta <prateekrungta@gmail.com>
2020-12-02 22:42:22 +0530

    Switch to 2 space tabs in existing JS files

--- assets/js/fontloader.js
+++ assets/js/fontloader.js
@@ -27,75 +27,75 @@
 **/
 
 (function (w) {
-    'use strict';
+  'use strict';
+  
+  var fontsets = [{
+      fonts: {
+      },
+      activeClass: 'wf-active',
+      cacheKey: 'wf-cached',
+    }],
+    timeout = 5*60*1000; // give it 5 mins before timing out
+  
+  // hasClass polyfill
+  // from http://youmightnotneedjquery.com/#has_class
+  function hasClass(el, className) {
+    if (el.classList) {
+      return el.classList.contains(className);
+    }
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+  }
+  
+  function isSessionStorageSupported() {
+    try {
+      var mod = 'test';
+      sessionStorage.setItem(mod, mod);
+      sessionStorage.removeItem(mod);
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+  
+  function loadFonts(fontsets) {
     
-    var fontsets = [{
-            fonts: {
-            },
-            activeClass: 'wf-active',
-            cacheKey: 'wf-cached',
-        }],
-        timeout = 5*60*1000; // give it 5 mins before timing out
+    // get the first fontset
+    if (fontsets.length === 0) return;
+    var config = fontsets.shift();
     
-    // hasClass polyfill
-    // from http://youmightnotneedjquery.com/#has_class
-    function hasClass(el, className) {
-        if (el.classList) {
-            return el.classList.contains(className);
-        }
-        return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+    function activate() {
+      // add a class to the document indicating the fonts have loaded
+      w.document.documentElement.className += ' ' + config.activeClass;
+      
+      // set a flag to optimise future visits
+      if (isSessionStorageSupported()) sessionStorage.setItem(config.cacheKey, true);
+      
+      // recurse over remaining fontsets
+      loadFonts(fontsets);
+      
+      // fire resize event to trigger dimension recalculations
+      if (window.jQuery) { jQuery(window).resize(); }
     }
     
-    function isSessionStorageSupported() {
-        try {
-            var mod = 'test';
-            sessionStorage.setItem(mod, mod);
-            sessionStorage.removeItem(mod);
-            return true;
-        } catch(e) {
-            return false;
-        }
-    }
+    // if we do not have FontFaceObserver, Promise, or ECMAScript 5, activate & fail
+    if (!('FontFaceObserver' in w) || !('Promise' in w) || !('keys' in Object)) return activate();
     
-    function loadFonts(fontsets) {
-        
-        // get the first fontset
-        if (fontsets.length === 0) return;
-        var config = fontsets.shift();
-        
-        function activate() {
-            // add a class to the document indicating the fonts have loaded
-            w.document.documentElement.className += ' ' + config.activeClass;
-            
-            // set a flag to optimise future visits
-            if (isSessionStorageSupported()) sessionStorage.setItem(config.cacheKey, true);
-            
-            // recurse over remaining fontsets
-            loadFonts(fontsets);
-            
-            // fire resize event to trigger dimension recalculations
-            if (window.jQuery) { jQuery(window).resize(); }
-        }
-        
-        // if we do not have FontFaceObserver, Promise, or ECMAScript 5, activate & fail
-        if (!('FontFaceObserver' in w) || !('Promise' in w) || !('keys' in Object)) return activate();
-        
-        // if the class is already set, we're good.
-        if (hasClass(w.document.documentElement, config.activeClass)) return activate();
-        
-        // if the fonts are already cached by the browser, activate them
-        if (isSessionStorageSupported() && sessionStorage.getItem(config.cacheKey)) return activate();
-        
-        // loop over font list and create an observer for each weight
-        var observers = [];
-        Object.keys(config.fonts).forEach(function (fontFamily) {
-            config.fonts[fontFamily].forEach(function (fontProperties) {
-                var new_observer = new w.FontFaceObserver(fontFamily, fontProperties);
-                observers.push(new_observer.load(config.testString || null, timeout));
-            });
-        });
-        w.Promise.all(observers).then(activate);
-    }
+    // if the class is already set, we're good.
+    if (hasClass(w.document.documentElement, config.activeClass)) return activate();
     
-    loadFonts(fontsets);
+    // if the fonts are already cached by the browser, activate them
+    if (isSessionStorageSupported() && sessionStorage.getItem(config.cacheKey)) return activate();
+    
+    // loop over font list and create an observer for each weight
+    var observers = [];
+    Object.keys(config.fonts).forEach(function (fontFamily) {
+      config.fonts[fontFamily].forEach(function (fontProperties) {
+        var new_observer = new w.FontFaceObserver(fontFamily, fontProperties);
+        observers.push(new_observer.load(config.testString || null, timeout));
+      });
+    });
+    w.Promise.all(observers).then(activate);
+  }
+  
+  loadFonts(fontsets);
 }(this));
--- assets/js/pjaxinit.js
+++ assets/js/pjaxinit.js
@@ -5,50 +5,50 @@
 // - http://buunguyen.github.io/topbar/
 //
 (function (window) {
-    var pjax = new Pjax({
-        elements: 'a[href]:not(.no-pjax)',
-        selectors: [
-            '[data-pjax-track]',
-            'title',
-            '#pjax-page',
-        ],
-        switches: {
-            '[data-pjax-track]': function (oldEl, newEl, options) {
-                if (oldEl.outerHTML == newEl.outerHTML) {
-                    // nothing has changed, continue as usual
-                    this.onSwitch();
-                } else {
-                    // something has changed. Abort.
-                    window.location.href = options.request.responseURL;
-                }
-            },
-        },
-        cacheBust: false,
-    });
-    
-    // Override handleResponse to treat 404 responses similar to 200 responses
-    pjax._handleResponse = pjax.handleResponse;
-    pjax.handleResponse = function (responseText, request, href, options) {
-      if (request.status === 404) {
-        return pjax._handleResponse(request.responseText, request, href, options);
-      }
-      return pjax._handleResponse(responseText, request, href, options);
-    };
-    
-    // Fire app's main() function on a new page load
-    if (window.main) {
-        document.addEventListener('pjax:success', main);
+  var pjax = new Pjax({
+    elements: 'a[href]:not(.no-pjax)',
+    selectors: [
+      '[data-pjax-track]',
+      'title',
+      '#pjax-page',
+    ],
+    switches: {
+      '[data-pjax-track]': function (oldEl, newEl, options) {
+        if (oldEl.outerHTML == newEl.outerHTML) {
+          // nothing has changed, continue as usual
+          this.onSwitch();
+        } else {
+          // something has changed. Abort.
+          window.location.href = options.request.responseURL;
+        }
+      },
+    },
+    cacheBust: false,
+  });
+  
+  // Override handleResponse to treat 404 responses similar to 200 responses
+  pjax._handleResponse = pjax.handleResponse;
+  pjax.handleResponse = function (responseText, request, href, options) {
+    if (request.status === 404) {
+      return pjax._handleResponse(request.responseText, request, href, options);
     }
-    
-    // Loading progress indicator
-    topbar.config({
-        barThickness: 1,
-        barColors: {
-            '0': '#000000',
-            '1': '#000000',
-        },
-        shadowBlur: 3,
-    });
-    document.addEventListener('pjax:send', topbar.show);
-    document.addEventListener('pjax:complete', topbar.hide);
+    return pjax._handleResponse(responseText, request, href, options);
+  };
+  
+  // Fire app's main() function on a new page load
+  if (window.main) {
+      document.addEventListener('pjax:success', main);
+  }
+  
+  // Loading progress indicator
+  topbar.config({
+    barThickness: 1,
+    barColors: {
+      '0': '#000000',
+      '1': '#000000',
+    },
+    shadowBlur: 3,
+  });
+  document.addEventListener('pjax:send', topbar.show);
+  document.addEventListener('pjax:complete', topbar.hide);
 }(this));
