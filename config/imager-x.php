b316fa8da9adeed34691b01ae7a691aacd15661c
Prateek Rungta <prateekrungta@gmail.com>
2022-08-18 19:05:53 +0530

    Fix Imager WebP config on dev
    Only use a custom WebP encoder on dev when `IMAGER_CWEBP_PATH` is defined

--- config/imager-x.php
+++ config/imager-x.php
@@ -9,55 +9,55 @@
 use craft\helpers\App;
 use craft\helpers\ArrayHelper;
 
-$defaultConfig = [
-    'imagerSystemPath' => '@assetBasePath/imager/',
-    'imagerUrl' => '@assetBaseUrl/imager/',
-    
-    'jpegQuality' => 85,
-    'allowUpscale' => false,
-    'interlace' => 'plane',
-    'removeMetadata' => true,
-    'cacheDuration' => 60*60*24*365*10, // 10 years
-    
-    'customEncoders' => [
-        'webp' => [
-            'path' => '/usr/bin/cwebp',
-            'options' => [
-                'quality' => 80,
-                'effort' => 4,
-            ],
-            'paramsString' => '-q {quality} -m {effort} {src} -o {dest}'
+$defaultCustomEncoders = [
+    'webp' => [
+        'path' => '/usr/bin/cwebp',
+        'options' => [
+            'quality' => 80,
+            'effort' => 4,
         ],
+        'paramsString' => '-q {quality} -m {effort} {src} -o {dest}'
     ],
-    
-    'optimizerConfig' => [
-        'jpegoptim' => [
-            'optionString' => '-s -m85 -T1',
-            'extensions' => ['jpg'],
-            'path' => '/usr/bin/jpegoptim',
-        ],
-        'optipng' => [
-            'extensions' => ['png'],
-            'path' => '/usr/bin/optipng',
-            'optionString' => '-o2',
-        ],
-    ],
-    
-   // 'clearKey' => '',
 ];
 
 return [
     // Global settings
-    '*' => $defaultConfig,
+    '*' => [
+        'imagerSystemPath' => '@assetBasePath/imager/',
+        'imagerUrl' => '@assetBaseUrl/imager/',
+        
+        'jpegQuality' => 85,
+        'allowUpscale' => false,
+        'interlace' => 'plane',
+        'removeMetadata' => true,
+        'cacheDuration' => 60*60*24*365*10, // 10 years
+        
+        'optimizerConfig' => [
+            'jpegoptim' => [
+                'optionString' => '-s -m85 -T1',
+                'extensions' => ['jpg'],
+                'path' => '/usr/bin/jpegoptim',
+            ],
+            'optipng' => [
+                'extensions' => ['png'],
+                'path' => '/usr/bin/optipng',
+                'optionString' => '-o2',
+            ],
+        ],
+        
+       // 'clearKey' => '',
+    ],
     
     // Production
     'production' => [
         'optimizers' => ['jpegoptim', 'optipng'],
+        'customEncoders' => $defaultCustomEncoders,
     ],
     
     // Staging
     'staging' => [
         'optimizers' => ['jpegoptim', 'optipng'],
+        'customEncoders' => $defaultCustomEncoders,
     ],
     
     // Dev
@@ -65,7 +65,7 @@ return [
         'hashPath' => true,
         'filenamePattern' => '{fullname}.{extension}',
         'customEncoders' => App::env('IMAGER_CWEBP_PATH')
-            ? ArrayHelper::merge($defaultConfig['customEncoders'], [
+            ? ArrayHelper::merge($defaultCustomEncoders, [
                 'webp' => [
                     'path' => App::env('IMAGER_CWEBP_PATH'),
                 ],
