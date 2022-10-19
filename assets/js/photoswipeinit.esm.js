21e2b09ce55a041b288ebd0614dd4fba49ac82b9
Anita <anita.chauhan100@gmail.com>
2022-10-19 14:13:41 +0530

    Include photoswipe javascript file

---
+++ assets/js/photoswipeinit.esm.js
@@ -0,0 +1,38 @@
+// Init Photoswipe.js
+// es6
+
+const options = {
+  gallery: 'body',
+  children: '.photoswipe',
+  pswpModule: () => import(window.PHOTOZOOM.photoswipe.module),
+};
+let lightbox;
+let instance;
+
+function getLightbox() {
+  if (!lightbox) {
+    lightbox = import(window.PHOTOZOOM.photoswipe.lightbox).then(
+      (module) => module.default
+    );
+  }
+  return lightbox;
+}
+
+function initPhotoswipe() {
+  if (document.querySelector(options.gallery)) {
+    getLightbox()
+      .then((lightbox) => {
+        if (instance) {
+          instance.destroy();
+        }
+        instance = new lightbox(options);
+        return instance;
+      })
+      .then((instance) => {
+        instance.init();
+      });
+  }
+}
+
+initPhotoswipe();
+document.addEventListener('pjax:success', initPhotoswipe);
