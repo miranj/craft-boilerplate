8d4331446cb0b1105a8b90c931df385630dd228e
Prateek Rungta <prateekrungta@gmail.com>
2023-06-21 15:27:07 +0530

    Remove nginx config for Redirects app
    Now natively handled by RunCloud domain config

--- serverconfigs/envs/production/setup.sh
+++ serverconfigs/envs/production/setup.sh
@@ -6,7 +6,6 @@
 
 APP_NAME="boilerplate-craft"
 CDN_NAME="boilerplate-cdn"
-REDIRECTS_NAME="boilerplate-redirects"
 
 REPO_DIR="/home/runcloud/webapps/$APP_NAME"
 TARGET_DIR="/etc/nginx-rc/extra.d"
@@ -24,7 +23,3 @@ ln -s $REPO_DIR/serverconfigs/envs/production/env.conf $APP_NAME.location.main-b
 ln -s $REPO_DIR/serverconfigs/nginx/craft/main.conf $APP_NAME.location.main.include.conf
 ln -s $REPO_DIR/serverconfigs/nginx/cdn/static.conf $APP_NAME.location.static.include.conf
 ln -s $REPO_DIR/serverconfigs/nginx/craft/proxy.conf $APP_NAME.location.proxy.include.conf
-
-# Redirects
-ln -s $REPO_DIR/serverconfigs/envs/production/env.conf $REDIRECTS_NAME.location.main-before.include.conf
-ln -s $REPO_DIR/serverconfigs/nginx/redirects/main.conf $REDIRECTS_NAME.location.main.include.conf
