9bcb815b180ef57c33ebb22326bb071b1a2da042
Prateek Rungta <prateekrungta@gmail.com>
2024-07-18 01:17:47 +0530

    Add sample Imager named transform preset for _image

---
+++ config/imager-x-transforms.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Configuration file for Imager Named Transforms
+ * https://imager-x.spacecat.ninja/configuration.html#configuring-named-transforms
+ */
+
+return [
+    'content' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 600],
+            ['width' => 1200],
+            ['width' => 1600],
+            ['width' => 1800],
+        ],
+    ],
+];
