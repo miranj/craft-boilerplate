f8e2c5bdb5eaaffa09e0c0606653a6c7aabc6122
Prateek Rungta <prateekrungta@gmail.com>
2024-07-22 23:04:11 +0530

    Switch to Imager’s quick syntax for transforms

--- config/imager-x-transforms.php
+++ config/imager-x-transforms.php
@@ -2,27 +2,24 @@
 
 /**
  * Configuration file for Imager Named Transforms
- * https://imager-x.spacecat.ninja/configuration.html#configuring-named-transforms
+ * https://imager-x.spacecat.ninja/usage/named-transforms.html
+ *
+ * eg:
+ *      // auto-fills intermediate transforms between 200w-800w, and
+ *      // crops the image to 16/9 ratio. (see docs for full syntax)
+ *      // https://imager-x.spacecat.ninja/usage/#quick-syntax
+ *
+ *      'thumbnail' => [
+ *          'transforms' => [200, 800, 16/9],
+ *      ],
  */
 
 return [
     'content' => [
-        'transforms' => [
-            ['width' => 400],
-            ['width' => 600],
-            ['width' => 1200],
-            ['width' => 1600],
-            ['width' => 1800],
-        ],
+        'transforms' => [400, 1800],
     ],
 
     'logo' => [
-        'transforms' => [
-            ['width' => 120],
-            ['width' => 200],
-            ['width' => 320],
-            ['width' => 460],
-            ['width' => 600],
-        ],
+        'transforms' => [120, 600],
     ],
 ];
