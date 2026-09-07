717eb643d5edec0f808162baaa2b443b3555cfa9
Anita <anita.chauhan100@gmail.com>
2026-09-07 13:18:20 +0530

    Include alpine plugins in build

--- assets/paths.js
+++ assets/paths.js
@@ -90,6 +90,9 @@ paths.tasks = {
     deferred: {
       source: [
         '../node_modules/@alpinejs/collapse/dist/cdn.js',
+        '../node_modules/@alpinejs/focus/dist/cdn.js',
+        '../node_modules/@alpinejs/intersect/dist/cdn.js',
+        '../node_modules/@alpinejs/resize/dist/cdn.js',
         '../node_modules/alpinejs/dist/cdn.js',
       ],
       destination: 'deferred.js',
