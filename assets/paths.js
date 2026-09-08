17a46f050d551fbfd4d69b3c7b9e861c5a7836cb
Anita <anita.chauhan100@gmail.com>
2026-09-08 13:19:06 +0530

    Remove alpine resize plugin

--- assets/paths.js
+++ assets/paths.js
@@ -92,7 +92,6 @@ paths.tasks = {
         '../node_modules/@alpinejs/collapse/dist/cdn.js',
         '../node_modules/@alpinejs/focus/dist/cdn.js',
         '../node_modules/@alpinejs/intersect/dist/cdn.js',
-        '../node_modules/@alpinejs/resize/dist/cdn.js',
         '../node_modules/alpinejs/dist/cdn.js',
       ],
       destination: 'deferred.js',
