c38141850159431c42a56d8984df1d7a5021fcac
Prateek Rungta <prateekrungta@gmail.com>
2026-09-11 01:25:49 +0200

    Disable auto spaces around dashes

--- config/typogrify.php
+++ config/typogrify.php
@@ -7,6 +7,9 @@ return [
     'set_hyphenation' => false,
     'set_space_collapse' => false,
 
+    // enables/disables wrapping of Em and En dashes in thin spaces.
+    'set_dash_spacing' => false,
+
     // establishes maximum length of a widows that will be protected
     'set_max_dewidow_length' => 15,
 
